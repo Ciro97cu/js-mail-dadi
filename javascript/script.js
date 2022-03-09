ec87e822b24b35e7abe7526d916e78098bb279e2
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-09 14:33:50 +0100

    completed first exercise

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,21 @@
-let prova = "Everything is Working";
-console.log(prova)
+const buttonMail = document.querySelector("#mail > button");
+buttonMail.addEventListener("click", function () {
+
+    const mailList = ["ciro.cusati@gmail.com", "antonio.cusati@gmail.com"];
+
+    const mailInput = document.querySelector("#mail > input");
+
+    const mailValue = mailInput.value;
+
+    for (let i = 0; i < mailList.length; i++) {
+
+        if (mailValue === mailList[i]) {
+            alert("You can enter");
+            break;
+        } else {
+            alert("You cannot enter");
+            break;
+        }
+    }
+
+})
